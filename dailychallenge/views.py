b4e9333c6a6614034860b26ccc7f85f9016836ae
Louie <63164377+levetation@users.noreply.github.com>
2023-06-07 22:11:50 +0100

    First Commit
    Django application set-up to use OpenAI to generate python puzzle and solution on POST

---
+++ dailychallenge/views.py
@@ -0,0 +1,27 @@
+from django.shortcuts import render, redirect
+import openai
+
+def ChatbotQuery(message):
+    openai.api_key = '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'
+    messages = [{"role": "system", "content": "You are an intelligent assistant."}]
+    chat = None  # Default value for chat
+    if message:
+        messages.append({"role": "user", "content": message})
+        chat = openai.ChatCompletion.create(model="gpt-3.5-turbo", messages=messages)
+        reply = chat.choices[0].message.content
+        messages.append({"role": "assistant", "content": reply})
+    else:
+        reply = "No message provided"  # Handle case when message is empty
+    return reply
+
+def DailyChallengeView(request):
+    context = {}
+    query = "Write a medium difficulty Python puzzle. Please only start your response with 'Here is the Python puzzle:' "
+    solution = "Write the solution code to the last puzzle. Do not include explanation just code. Please only start your response with 'Here is the Python solution:'"
+    if request.method == 'POST' and 'generate_puzzle' in request.POST:
+        puzzle = ChatbotQuery(query)
+        solution = ChatbotQuery(solution)
+        context['puzzle'] = puzzle
+        context['solution'] = solution
+
+    return render(request, 'dailychallenge/dailychallenge.html', context)
